ebb1a7f2c17a33eefad07d423652de8f70fa15d7
abiola <oladunjoye1998@gmail.com>
2024-03-30 06:32:38 +0100

    amount for traceid

--- wallet/views.py
+++ wallet/views.py
@@ -320,6 +320,6 @@ class BankTransfer(APIView):
                 trans.reference_id=result["traceId"]
                 trans.save()
                 Thread(target=send_debit_mail, args=[request.user.email,{"sender":f"{request.user.first_name} {request.user.last_name}","time":datetime.now(),"transId":trans_id,"amount":amount}]).start()
-            return  successResponse(id,"amount transferred","data",result)
+            return  successResponse(id,"amount transferred","data",{"sender":f"{request.user.first_name} {request.user.last_name}","time":datetime.now(),"transId":trans_id,"amount":amount,"chargeFee":charge["chargeFee"]})
         else:
             return errorResponse(id,'Missing required parameters')
