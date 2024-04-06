fed41a2d8e07777cd935e9a2fac81bb1efe2113a
abiola <oladunjoye1998@gmail.com>
2024-04-06 10:54:20 +0100

    d

--- billPayment/permissions.py
+++ billPayment/permissions.py
@@ -2,6 +2,8 @@ from rest_framework.permissions import BasePermission
 from wallet.models import Wallet
 from user.utils import log_request,send_password_reset_mail,send_activation_mail,send_activation_phone,validatingPassword,checkRequest,errorResponse,successResponse
 from rest_framework.response import Response
+from rest_framework import status
+
 
 class IsWallet(BasePermission):
     def has_permission(self, request, view):
