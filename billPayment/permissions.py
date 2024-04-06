ead43d69bd45172a5609f6560501d00fb6fc42d1
abiola <oladunjoye1998@gmail.com>
2024-04-06 10:50:20 +0100

    d

--- billPayment/permissions.py
+++ billPayment/permissions.py
@@ -1,13 +1,14 @@
 from rest_framework.permissions import BasePermission
 from wallet.models import Wallet
-
+from user.utils import log_request,send_password_reset_mail,send_activation_mail,send_activation_phone,validatingPassword,checkRequest,errorResponse,successResponse
+import uuid
 
 class IsWallet(BasePermission):
     def has_permission(self, request, view):
         try:
             walletUser = Wallet.objects.get(user=request.user,currency_code="NGN")
-        except walletUser.DoesNotExist:
-            return False
+        except Wallet.DoesNotExist:
+            return errorResponse(str(uuid.uuid4[:6]),"please create your wallet")
   
         return True
 
