e2b63c1c99430e61eb14bfba9cd5614f71d25fe9
abiola <oladunjoye1998@gmail.com>
2024-04-06 10:51:39 +0100

    d

--- billPayment/permissions.py
+++ billPayment/permissions.py
@@ -1,14 +1,15 @@
 from rest_framework.permissions import BasePermission
 from wallet.models import Wallet
+from django.
 from user.utils import log_request,send_password_reset_mail,send_activation_mail,send_activation_phone,validatingPassword,checkRequest,errorResponse,successResponse
-import uuid
+from rest_framework.response import Response
 
 class IsWallet(BasePermission):
     def has_permission(self, request, view):
         try:
             walletUser = Wallet.objects.get(user=request.user,currency_code="NGN")
         except Wallet.DoesNotExist:
-            return errorResponse(str(uuid.uuid4[:6]),"please create your wallet")
+            return Response({"error": "Wallet does not exist"}, status=status.HTTP_403_FORBIDDEN)
   
         return True
 
